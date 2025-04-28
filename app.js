a18fb70d6b2555c79cdcfa0814991b879a93f5bd
bridgetorr17 <bridgetorr1902@gmail.com>
2025-04-28 19:36:44 -0400

    refactoring get

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const {MongoClient, ServerApiVersion} = require('mongodb');
+require('dotenv').config()
+
+const app = express();
+const PORT = 8000;
+const mongoURI = process.env.MONGO_URI;
+const client = new MongoClient(mongoURI);
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async (req, res) => {
+    try{
+        await client.connect()
+
+        let concertCollection = client.db('SupriseSongs').collection('Concerts');
+        let results = await concertCollection.find().toArray()
+        
+        res.send(JSON.stringify(results));
+    }
+    catch(error){
+        console.error(error);
+    }
+    finally{
+        await client.close();
+    }
+});
+
+app.get('/about', (req, res) => {
+    res.send('about route');
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
