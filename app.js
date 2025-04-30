dec18eaef55f9c28f3f4c77e4c48d8c17b3911c0
bridgetorr17 <bridgetorr1902@gmail.com>
2025-04-30 17:45:49 -0400

    POST and PUT refactoring

--- app.js
+++ app.js
@@ -28,8 +28,46 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.post('/addConcert', (req, res) => {
+app.post('/addConcert', async (req, res) => {
     const concert = req.body;
+    concert['votes'] = 0;
+
+    try{
+        await client.connect();
+
+        let concertCollection = client.db('SupriseSongs').collection('Concerts');
+        await concertCollection.insertOne(concert);
+        res.redirect('/');
+    }
+    catch(error){
+        console.error(error);
+    }
+    finally{
+        await client.close();
+    }
+});
+
+app.put('/like', async (req, res) => {
+    const concert = req.body.concert.trim();
+    
+    try{
+        await client.connect();
+
+        let concertCollection = client.db('SupriseSongs').collection('Concerts');
+        let result = await concertCollection.findOneAndUpdate(
+            { concertName: concert},
+            { $inc: {votes: 1}},
+            {returnDocument: 'after'} 
+        );
+
+        res.status(200).json(result.votes);
+    }
+    catch(error){
+        console.error(error);
+    }
+    finally{
+        await client.close();
+    }
 })
 
 app.listen(PORT, () => {
