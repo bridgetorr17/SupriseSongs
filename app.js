7c2829b339e1394a93f2aade240e1a2c885c2da2
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-04 14:09:47 -0400

    checking for real TS songs

--- app.js
+++ app.js
@@ -37,15 +37,48 @@ app.post('/addConcert', async (req, res) => {
         await client.connect();
 
         let concertCollection = client.db('SupriseSongs').collection('Concerts');
+        const albumCollection = client.db('SupriseSongs').collection('Albums');
 
         const alreadyExists = await concertCollection.findOne({concertName : concert['concertName']})
         console.log(concert['concertName']);
         console.log(`does this exist yet? ${alreadyExists}`);
 
         if(alreadyExists === null){
-            await concertCollection.insertOne(concert);
-            console.log('concert added');
-            res.status(201).send('Concert added');
+            //Check if they are actually Taylor Swift Songs
+            const aggGuitar = [ 
+                { '$match': 
+                    { 'tracklist': 
+                        {        
+                        '$regex' : concert['guitarSong'],
+                        '$options' : 'i'
+                        }
+                    } 
+                } 
+            ];
+            const aggPiano = [ 
+                { '$match': 
+                    { 'tracklist': 
+                        {        
+                        '$regex' : concert['pianoSong'],
+                        '$options' : 'i'
+                        }
+                    } 
+                } 
+            ];
+            
+            const cursorGuitar = await albumCollection.aggregate(aggGuitar).toArray();
+            const cursorPiano = await albumCollection.aggregate(aggPiano).toArray();
+
+            if(cursorPiano.length > 0 && cursorGuitar.length > 0){
+                await concertCollection.insertOne(concert);
+                console.log('concert added');
+                res.status(201).send('Concert added');
+            }
+            else{
+                console.log('not actual TS songs')
+                res.status(406).send('One of the songs entered were not TS songs');
+            }
+            
         }
         else{
             console.log('concert already exists in db');
